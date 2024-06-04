b3b405f5c530b24c32ac76a2fbacf0e40da8eb3f
var-dos <160048139+var-dos@users.noreply.github.com>
2024-06-04 13:51:54 +0000

    hhggf

--- data/scripts/miscscripts.js
+++ data/scripts/miscscripts.js
@@ -36,24 +36,20 @@ function checkPassword() {
 
     // Determine the correct password based on some condition
     // Replace placeholders with actual conditions or expressions
-  
     if (password === "vz611") {
       correctPassword = "vz611";
-      var someCondition = true; // Replace this with the actual condition
-      if (someCondition) {
-          passwordNumber = 1; // Set password number to 1
-      }
-  } else if (password === "c3112") {
+      passwordNumber = 1; // Set password number to 1
+    } else if (password === "c3112") {
       correctPassword = "c3112";
       passwordNumber = 2; // Set password number to 2
-  } else if (password === "t41190") {
+    } else if (password === "t41190") {
       correctPassword = "t41190";
       passwordNumber = 3; // Set password number to 3
-  } else if (password === "vz611") {
-      correctPassword = "vz611";
-      passwordNumber = 4; // Set password number to 4
-  }
-    
+    }
+      else if (password === "vz633") {
+        correctPassword = "vz633";
+        passwordNumber = 4; // Set password number to 4
+    }
   
     // Check if the entered password matches the correct password
     if (password === correctPassword) {
