df0de71f4b1b2a52933ec483baf8a6e97e807518
var-dos <160048139+var-dos@users.noreply.github.com>
2024-06-04 14:07:43 +0000

    test

--- data/scripts/miscscripts.js
+++ data/scripts/miscscripts.js
@@ -46,8 +46,8 @@ function checkPassword() {
       correctPassword = "t41190";
       passwordNumber = 3; // Set password number to 3
     }
-      else if (password === "vz633") {
-        correctPassword = "vz633";
+      else if (password === "vz611") {
+        correctPassword = "vz611";
         passwordNumber = 4; // Set password number to 4
     }
      else {
